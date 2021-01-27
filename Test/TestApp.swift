0da7590234ca37ad52547cfa9071520e5cdca105
underTheBlueSun <wind7129@naver.com>
2021-01-27 18:43:53 +0900

    Initial Commit

---
+++ Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by underTheBlueSun on 2021/01/27.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
